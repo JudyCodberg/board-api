43af38f1443fab05174debf957632bdc05249321
JudyCodberg <128333114+JudyCodberg@users.noreply.github.com>
2023-09-27 11:43:17 +0900

    feat : add cors setting

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const { UserRouter, BoardRouter, CommentRouter } = require("./routes/index.js");
 const handleError = require("./handleErr.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/user", UserRouter);
 app.use("/board", BoardRouter);
@@ -13,6 +15,6 @@ app.use("/comment", CommentRouter);
 
 app.use(handleError.handleError);
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Server is running");
 });
